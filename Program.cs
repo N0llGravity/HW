faad5459a44de7f003f582332d5995931f702f8e
Artem <ar.toropov@mail.ru>
2022-12-05 22:23:50 +0300

    MinMaX

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Console.WriteLine("input a first number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input a second number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+Console.WriteLine($"Min: {num1}");
+Console.Write($"Max: {num2}");
+*/
