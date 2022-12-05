59a1c6a1f5ed41f0e11ad8b766555afa21e5f495
Artem <ar.toropov@mail.ru>
2022-12-05 23:20:52 +0300

    Максимальное из 3х чисел

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿/*
+
+// Минимальное и максимальное число из 2х
+
 Console.WriteLine("input a first number ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("input a second number ");
@@ -14,3 +17,38 @@ if(num1 > num2)
 Console.WriteLine($"Min: {num1}");
 Console.Write($"Max: {num2}");
 */
+
+
+
+/* 
+
+// Максимальное число из 3х
+
+int Maximum(int first, int second, int third)
+{
+    int result = first;
+
+    if(result < second)
+    {
+    result = second;
+    }
+    
+    if(result < third)
+    {
+    result = third;
+    }
+
+    return result;
+}
+
+Console.WriteLine("input a first number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input a second number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input a third number ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int res = Maximum(num1, num2, num3);
+Console.Write($"Biggest number is {res}");
+*/
+
